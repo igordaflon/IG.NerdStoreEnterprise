2b421814f67a8d0a68a070b3a5c4d7e569f5d4a9
Igor Daflon <igor.couto@pgmais.com.br>
2022-04-30 21:43:46 -0300

    Catalogo HttpClient

--- src/Web/NSE.WebApp.MVC/Configuration/WebAppConfig.cs
+++ src/Web/NSE.WebApp.MVC/Configuration/WebAppConfig.cs
@@ -18,20 +18,16 @@ namespace NSE.WebApp.MVC.Configuration
 
         public static void UseMvcConfiguration(this IApplicationBuilder app, IWebHostEnvironment env)
         {
-            //if (env.IsDevelopment())
-            //{
-            //    app.UseDeveloperExceptionPage();
-            //}
-            //else
-            //{
-            //    app.UseExceptionHandler("/erro/500");
-            //    app.UseStatusCodePagesWithRedirects("/erro/{0}");
-            //    app.UseHsts();
-            //}
-
-            app.UseExceptionHandler("/erro/500");
-            app.UseStatusCodePagesWithRedirects("/erro/{0}");
-            app.UseHsts();
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/erro/500");
+                app.UseStatusCodePagesWithRedirects("/erro/{0}");
+                app.UseHsts();
+            }
 
             app.UseHttpsRedirection();
             app.UseStaticFiles();
@@ -46,7 +42,7 @@ namespace NSE.WebApp.MVC.Configuration
             {
                 endpoints.MapControllerRoute(
                     name: "default",
-                    pattern: "{controller=Home}/{action=Index}/{id?}");
+                    pattern: "{controller=Catalogo}/{action=Index}/{id?}");
             });
         }
     }
---
+++ src/Web/NSE.WebApp.MVC/Services/CatalogoService.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.Extensions.Options;
+using NSE.WebApp.MVC.Extensions;
+using NSE.WebApp.MVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace NSE.WebApp.MVC.Services
+{
+    public class CatalogoService : Service, ICatalogoService
+    {
+        private readonly HttpClient _httpClient;
+        private readonly AppSettings _settings;
+
+        public CatalogoService
+        (
+            HttpClient _httpClient,
+            IOptions<AppSettings> _settings
+        )
+        {
+            _httpClient.BaseAddress = new Uri(_settings.Value.CatalogoUrl);
+            this._httpClient = _httpClient;
+        }
+
+        public async Task<ProdutoViewModel> ObterPorId(Guid id)
+        {
+            var response = await _httpClient.GetAsync($"/catalogo/produtos/{id}");
+
+            TratarErrosResponse(response);
+
+            return await DeserializarObjetoResponse<ProdutoViewModel>(response);
+        }
+
+        public async Task<IEnumerable<ProdutoViewModel>> ObterTodos()
+        {
+            var response = await _httpClient.GetAsync("/catalogo/produtos");
+
+            TratarErrosResponse(response);
+
+            return await DeserializarObjetoResponse<IEnumerable<ProdutoViewModel>>(response);
+        }
+    }
+}
