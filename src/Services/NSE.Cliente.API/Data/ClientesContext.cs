5c084c8cd195bc3bf1a25ef64643b9a81770e5e6
Igor Daflon <igor.couto@pgmais.com.br>
2022-05-04 21:46:09 -0300

    Mapeamento das tabelas(Clientes)

---
+++ src/Services/NSE.Cliente.API/Data/ClientesContext.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NSE.Core.Data;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NSE.Cliente.API.Data
+{
+    public sealed class ClientesContext : DbContext, IUnitOfWork
+    {
+        public ClientesContext(DbContextOptions<ClientesContext> options)
+            : base(options)
+        {
+            ChangeTracker.QueryTrackingBehavior = QueryTrackingBehavior.NoTracking;
+            ChangeTracker.AutoDetectChangesEnabled = false;
+        }
+
+        public DbSet<Models.Cliente> Clientes { get; set; }
+        public DbSet<Models.Endereco> Enderecos { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            foreach (var relationship in modelBuilder.Model.GetEntityTypes()
+                .SelectMany(e => e.GetForeignKeys())) relationship.DeleteBehavior = DeleteBehavior.ClientSetNull;
+
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(ClientesContext).Assembly);
+        }
+
+        public async Task<bool> Commit()
+        {
+            var sucesso = await base.SaveChangesAsync() > 0;
+
+            return sucesso;
+        }
+    }
+}
