61912df33be337cd8638849e51b3f577909af9bb
Igor Daflon <igor.couto@pgmais.com.br>
2022-05-19 20:41:04 -0300

    Request Response Pattern

---
+++ src/Building Blocks/NSE.Core/Messages/Integration/ReponseMessage.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace NSE.Core.Messages.Integration
+{
+    public class ReponseMessage : Message
+    {
+        public ValidationResult ValidationResult { get; set; }
+
+        public ReponseMessage(ValidationResult validationResult)
+        {
+            ValidationResult = validationResult;
+        }
+    }
+}
