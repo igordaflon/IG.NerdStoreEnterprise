370b2153223c02db3afa39949db4b8145107f176
Igor Daflon <igor.couto@pgmais.com.br>
2022-06-01 22:23:44 -0300

    Regras de negócio Carrinho de Compras

---
+++ src/Building Blocks/NSE.WebApi.Core/Usuario/AspNetUser.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Security.Claims;
+
+namespace NSE.WebApi.Core.Usuario
+{
+
+    public class AspNetUser : IAspNetUser
+    {
+        private readonly IHttpContextAccessor _accessor;
+
+        public AspNetUser(IHttpContextAccessor _accessor)
+        {
+            this._accessor = _accessor;
+        }
+
+        public string Name => _accessor.HttpContext.User.Identity.Name;
+
+        public bool EstaAutenticado()
+        {
+            return _accessor.HttpContext.User.Identity.IsAuthenticated;
+        }
+
+        public IEnumerable<Claim> ObterClaims()
+        {
+            return _accessor.HttpContext.User.Claims;
+        }
+
+        public HttpContext ObterHttpContext()
+        {
+            return _accessor.HttpContext;
+        }
+
+        public string ObterUserEmail()
+        {
+            return EstaAutenticado() ? _accessor.HttpContext.User.GetUserEmail() : "";
+        }
+
+        public Guid ObterUserId()
+        {
+            return EstaAutenticado() ? Guid.Parse(_accessor.HttpContext.User.GetUserId()) : Guid.Empty;
+        }
+
+        public string ObterUsuarioToken()
+        {
+            return EstaAutenticado() ? _accessor.HttpContext.User.GetUserToken() : "";
+        }
+
+        public bool PossuiRole(string role)
+        {
+            return _accessor.HttpContext.User.IsInRole(role);
+        }
+    }
+}
--- src/Web/NSE.WebApp.MVC/Services/Handlers/HttpClientAuthorizationDelegatingHandler.cs
+++ src/Web/NSE.WebApp.MVC/Services/Handlers/HttpClientAuthorizationDelegatingHandler.cs
@@ -1,4 +1,4 @@
-﻿using NSE.WebApp.MVC.Extensions;
+﻿using NSE.WebApi.Core.Usuario;
 using System.Collections.Generic;
 using System.Net.Http;
 using System.Net.Http.Headers;
@@ -9,9 +9,9 @@ namespace NSE.WebApp.MVC.Services.Handlers
 {
     public class HttpClientAuthorizationDelegatingHandler : DelegatingHandler
     {
-        private readonly IUser _user;
+        private readonly IAspNetUser _user;
 
-        public HttpClientAuthorizationDelegatingHandler(IUser _user)
+        public HttpClientAuthorizationDelegatingHandler(IAspNetUser _user)
         {
             this._user = _user;
         }
