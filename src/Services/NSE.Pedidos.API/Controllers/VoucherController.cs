b601ea1d34a4465fb211204341becf4e7715e1f1
Igor Daflon <igor.couto@pgmais.com.br>
2022-06-14 00:30:40 -0300

    Implementação Voucher

---
+++ src/Services/NSE.Pedidos.API/Controllers/VoucherController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using NSE.WebApi.Core.Controllers;
+using System.Threading.Tasks;
+
+namespace NSE.Pedidos.API.Controllers
+{
+    [Authorize]
+    public class VoucherController : MainController
+    {
+        [HttpGet("voucher/{codigo}")]
+        public async Task<IActionResult> ObterPorCodigo(string codigo)
+        {
+            return View();
+        }
+    }
+}
